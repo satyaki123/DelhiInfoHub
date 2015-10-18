59f06cb3e68710f13ec3049b9e78c0e4a3872420
satyaki123 <satyaki@algofocus.com>
2015-10-18 13:19:26 +0530

    Added rank field in API response

--- app/controllers/places_data_controller.rb
+++ app/controllers/places_data_controller.rb
@@ -4,15 +4,15 @@ class PlacesDataController < ApplicationController
     case category
     when "dangerzone"
     place_data =  PlaceDatum.group("crime_weight").order("crime_weight desc")
-    places = place_data.limit(5).map{|p|{:name => p.place_name,:lat => p.lat,:lng => p.long,:weight => p.crime_weight}}
+    places = place_data.limit(5).each_with_index.map{|p,i|{:name => p.place_name,:lat => p.lat,:lng => p.long,:weight => p.crime_weight ,:rank => i+1}}
     render :json => {:success => true,:data => places}
     when "healthzone"
     place_data =  PlaceDatum.group("health_weight").order("health_weight desc").all
-    places = place_data.limit(5).map{|p|{:name => p.place_name,:lat => p.lat,:lng => p.long,:weight => p.health_weight}}
+    places = place_data.limit(5).each_with_index.map{|p,i|{:name => p.place_name,:lat => p.lat,:lng => p.long,:weight => p.health_weight, :rank => i+1}}
     render :json => {:success => true,:data => places}
     when "entertainmentzone"
     place_data =  PlaceDatum.group("entertainment_weight").order("entertainment_weight desc").all
-    places = place_data.limit(5).map{|p|{:name => p.place_name,:lat => p.lat,:lng => p.long,:weight => p.entertainment_weight}}
+    places = place_data.limit(5).each_with_index.map{|p,i|{:name => p.place_name,:lat => p.lat,:lng => p.long,:weight => p.entertainment_weight,:rank => i+1}}
     render :json => {:success => true,:data => places}
     else
     render :json => {:success => false,:data => "No data found"}
